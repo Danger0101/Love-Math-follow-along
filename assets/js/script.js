489236d3c4f76aa1d67f68f7870fc699e5a5001a
John Hummel <34178990+Danger0101@users.noreply.github.com>
2023-07-05 13:29:12 +0100

    Set up structure of game functions and added css
    Set up structure of game functions and added css

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function rungame() {}
+
+function checkAnswer() {}
+
+function calcCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractionQuestion() {}
+
+function displayMultiplicationQuestion() {}
+
+function displayDivisionQuestion() {}
